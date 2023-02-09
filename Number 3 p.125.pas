962905fa252766397087c821482edd458cdf9f4b
Saveliy Kozhevnikov-Grekin <124622646+whgfhg@users.noreply.github.com>
2023-02-09 19:34:11 +0300

    Add files via upload
    formule d=sqrt(sqr(xb-xa)+sqr(yb-ya))

---
+++ Number 3 p.125.pas
@@ -0,0 +1,9 @@
+﻿program f;
+var xa, ya, xb, yb, d, AB: real;
+begin
+writeln('Введите числа xb, xa, yb, ya: ');
+read (xa, ya, xb ,yb);
+d:=sqrt(sqr(xb-xa)+sqr(yb-ya));
+AB:=d;
+Write( 'Результат(AB): ', AB);
+end.
